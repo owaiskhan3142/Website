36d6d24991422624c49fa800969d7fdce38f195b
Mir Mubashir Ali <mubashirali12350@gmail.com>
2021-11-05 20:57:22 +0500

    new java script file is and a new page is added "services"

---
+++ shashkay.js
@@ -0,0 +1,14 @@
+document.getElementById('kunang1').style.left = "50%";
+document.getElementById('kunang1').style.top = "50%";
+
+document.getElementById('kunang2').style.left = "30%";
+document.getElementById('kunang2').style.top = "30%";
+
+document.getElementById('kunang3').style.left = "10%";
+document.getElementById('kunang3').style.top = "60%";
+
+document.getElementById('kunang4').style.left = "10%";
+document.getElementById('kunang4').style.top = "70%";
+
+document.getElementById('kunang5').style.left = "70%";
+document.getElementById('kunang5').style.top = "70%";
